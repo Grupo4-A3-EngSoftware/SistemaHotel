901235b77b31bc47b666fb3508a6b7c87d79c9e5
Giovannebatista <giovannebatista099@gmail.com>
2025-05-21 09:51:45 -0300

    Alteração do comboBox do FmCadastro

--- SistemaHotel/FmCadastro.Designer.cs
+++ SistemaHotel/FmCadastro.Designer.cs
@@ -48,9 +48,10 @@
             | System.Windows.Forms.AnchorStyles.Left)));
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(2, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(544, 640);
+            this.pictureBox1.Size = new System.Drawing.Size(363, 416);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -59,18 +60,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(602, 69);
+            this.label1.Location = new System.Drawing.Point(401, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(174, 29);
+            this.label1.Size = new System.Drawing.Size(116, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Crie uma conta";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(603, 125);
+            this.label2.Location = new System.Drawing.Point(402, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(151, 20);
+            this.label2.Size = new System.Drawing.Size(101, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Seu nome completo";
             // 
@@ -78,17 +81,19 @@
             // 
             this.txtnome.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtnome.Location = new System.Drawing.Point(607, 164);
+            this.txtnome.Location = new System.Drawing.Point(405, 107);
+            this.txtnome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtnome.Name = "txtnome";
-            this.txtnome.Size = new System.Drawing.Size(265, 26);
+            this.txtnome.Size = new System.Drawing.Size(178, 20);
             this.txtnome.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(603, 220);
+            this.label3.Location = new System.Drawing.Point(402, 143);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(120, 20);
+            this.label3.Size = new System.Drawing.Size(80, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Crie uma senha";
             // 
@@ -96,18 +101,20 @@
             // 
             this.txtsenha.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtsenha.Location = new System.Drawing.Point(607, 268);
+            this.txtsenha.Location = new System.Drawing.Point(405, 174);
+            this.txtsenha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtsenha.Name = "txtsenha";
             this.txtsenha.PasswordChar = '*';
-            this.txtsenha.Size = new System.Drawing.Size(265, 26);
+            this.txtsenha.Size = new System.Drawing.Size(178, 20);
             this.txtsenha.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(603, 331);
+            this.label4.Location = new System.Drawing.Point(402, 215);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(121, 20);
+            this.label4.Size = new System.Drawing.Size(80, 13);
             this.label4.TabIndex = 2;
             this.label4.Text = "Confirma senha";
             // 
@@ -115,41 +122,45 @@
             // 
             this.txtsenhaconf.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtsenhaconf.Location = new System.Drawing.Point(607, 385);
+            this.txtsenhaconf.Location = new System.Drawing.Point(405, 250);
+            this.txtsenhaconf.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtsenhaconf.Name = "txtsenhaconf";
             this.txtsenhaconf.PasswordChar = '*';
-            this.txtsenhaconf.Size = new System.Drawing.Size(265, 26);
+            this.txtsenhaconf.Size = new System.Drawing.Size(178, 20);
             this.txtsenhaconf.TabIndex = 5;
             // 
             // btnconta
             // 
             this.btnconta.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnconta.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.btnconta.Location = new System.Drawing.Point(767, 501);
+            this.btnconta.Location = new System.Drawing.Point(511, 326);
+            this.btnconta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnconta.Name = "btnconta";
-            this.btnconta.Size = new System.Drawing.Size(172, 42);
+            this.btnconta.Size = new System.Drawing.Size(115, 27);
             this.btnconta.TabIndex = 6;
             this.btnconta.Text = "Registrar conta";
             this.btnconta.UseVisualStyleBackColor = false;
             // 
             // comboBox1
             // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Cliente",
             "Funcionários"});
-            this.comboBox1.Location = new System.Drawing.Point(932, 162);
+            this.comboBox1.Location = new System.Drawing.Point(621, 105);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.Size = new System.Drawing.Size(82, 21);
             this.comboBox1.TabIndex = 7;
-            this.comboBox1.Text = "Cliente";
             // 
             // Cadastro
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1153, 643);
+            this.ClientSize = new System.Drawing.Size(769, 418);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btnconta);
             this.Controls.Add(this.txtsenhaconf);
@@ -160,6 +171,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Cadastro";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro";
