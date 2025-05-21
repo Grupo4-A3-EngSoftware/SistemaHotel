fb15decf6f68e50da6b4c32cf7ac715f8eb00a42
Giovannebatista <giovannebatista099@gmail.com>
2025-05-21 10:19:49 -0300

    Ajustes na interface do formulário de cadastro
    Adiciona um DataGridView para exibir hóspedes cadastrados.
    Ajusta propriedades de localização e tamanho de componentes
    para melhorar o layout, reduzindo margens e tamanhos.
    A dimensão total do formulário também foi alterada.

--- SistemaHotel/FmCadastroHodped.Designer.cs
+++ SistemaHotel/FmCadastroHodped.Designer.cs
@@ -38,65 +38,76 @@
             this.label4 = new System.Windows.Forms.Label();
             this.txtemail = new System.Windows.Forms.TextBox();
             this.btnsalvar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 27);
+            this.label1.Location = new System.Drawing.Point(17, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome";
             // 
             // txtnome
             // 
             this.txtnome.Enabled = false;
-            this.txtnome.Location = new System.Drawing.Point(29, 50);
+            this.txtnome.Location = new System.Drawing.Point(19, 32);
+            this.txtnome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtnome.Name = "txtnome";
-            this.txtnome.Size = new System.Drawing.Size(456, 26);
+            this.txtnome.Size = new System.Drawing.Size(305, 20);
             this.txtnome.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 125);
+            this.label2.Location = new System.Drawing.Point(17, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Endereço";
             // 
             // txtendereco
             // 
             this.txtendereco.Enabled = false;
-            this.txtendereco.Location = new System.Drawing.Point(29, 158);
+            this.txtendereco.Location = new System.Drawing.Point(19, 103);
+            this.txtendereco.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtendereco.Multiline = true;
             this.txtendereco.Name = "txtendereco";
-            this.txtendereco.Size = new System.Drawing.Size(456, 135);
+            this.txtendereco.Size = new System.Drawing.Size(305, 89);
             this.txtendereco.TabIndex = 3;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.panel1.Location = new System.Drawing.Point(29, 388);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(19, 252);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1231, 195);
+            this.panel1.Size = new System.Drawing.Size(821, 127);
             this.panel1.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(25, 358);
+            this.label3.Location = new System.Drawing.Point(17, 233);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(174, 20);
+            this.label3.Size = new System.Drawing.Size(116, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Hóspedes cadastrados";
             // 
             // btnnovo
             // 
-            this.btnnovo.Location = new System.Drawing.Point(223, 621);
+            this.btnnovo.Location = new System.Drawing.Point(149, 404);
+            this.btnnovo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnnovo.Name = "btnnovo";
-            this.btnnovo.Size = new System.Drawing.Size(115, 49);
+            this.btnnovo.Size = new System.Drawing.Size(77, 32);
             this.btnnovo.TabIndex = 6;
             this.btnnovo.Text = "Novo";
             this.btnnovo.UseVisualStyleBackColor = true;
@@ -105,36 +116,47 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(674, 27);
+            this.label4.Location = new System.Drawing.Point(449, 18);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.Size = new System.Drawing.Size(32, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "Email";
             // 
             // txtemail
             // 
             this.txtemail.Enabled = false;
-            this.txtemail.Location = new System.Drawing.Point(678, 50);
+            this.txtemail.Location = new System.Drawing.Point(452, 32);
+            this.txtemail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtemail.Name = "txtemail";
-            this.txtemail.Size = new System.Drawing.Size(351, 26);
+            this.txtemail.Size = new System.Drawing.Size(235, 20);
             this.txtemail.TabIndex = 8;
             // 
             // btnsalvar
             // 
-            this.btnsalvar.Location = new System.Drawing.Point(404, 621);
+            this.btnsalvar.Location = new System.Drawing.Point(269, 404);
+            this.btnsalvar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnsalvar.Name = "btnsalvar";
-            this.btnsalvar.Size = new System.Drawing.Size(119, 49);
+            this.btnsalvar.Size = new System.Drawing.Size(79, 32);
             this.btnsalvar.TabIndex = 9;
             this.btnsalvar.Text = "Salvar";
             this.btnsalvar.UseVisualStyleBackColor = true;
             this.btnsalvar.Click += new System.EventHandler(this.btnsalvar_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 15);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(789, 96);
+            this.dataGridView1.TabIndex = 0;
+            // 
             // FmCadastroHodped
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(1299, 708);
+            this.ClientSize = new System.Drawing.Size(866, 460);
             this.Controls.Add(this.btnsalvar);
             this.Controls.Add(this.txtemail);
             this.Controls.Add(this.label4);
@@ -145,9 +167,12 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtnome);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FmCadastroHodped";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de Hóspede";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,5 +190,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtemail;
         private System.Windows.Forms.Button btnsalvar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
