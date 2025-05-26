7d04b27fac1580aa1dee08cf06cd00ec0cbf4d20
Giovannebatista <giovannebatista099@gmail.com>
2025-05-26 10:25:50 -0300

    Ajustes de layout e tamanhos na interface
    Alterações no `FmGestaopagamentos.Designer.cs`:
    - Redução das posições e tamanhos de `dataGridView1`, `comboBox1`, `btnpagamento`, `label1` e `label2`, resultando em uma interface mais compacta.
    - Adição de margens a alguns componentes e ajuste do texto do `label1`.
    - Dimensão da janela principal reduzida.
    
    Alterações no `FmLogin.Designer.cs`:
    - Ajustes nas posições e tamanhos de `user`, `txtusuario`, `senha`, `txtsenha`, `btnLogin` e `btnCadastro`, com localizações mais próximas do canto superior esquerdo.
    - Dimensão da janela principal reduzida e margens adicionadas a alguns componentes.
    - Ajuste do texto dos rótulos para tamanhos de fonte menores.

--- SistemaHotel/FmGestaopagamentos.Designer.cs
+++ SistemaHotel/FmGestaopagamentos.Designer.cs
@@ -40,11 +40,12 @@
             // 
             this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(88, 53);
+            this.dataGridView1.Location = new System.Drawing.Point(59, 34);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1184, 245);
+            this.dataGridView1.Size = new System.Drawing.Size(789, 363);
             this.dataGridView1.TabIndex = 0;
             // 
             // comboBox1
@@ -56,17 +57,19 @@
             "Dinheiro",
             "Cartão",
             "Boleto"});
-            this.comboBox1.Location = new System.Drawing.Point(563, 313);
+            this.comboBox1.Location = new System.Drawing.Point(361, 417);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(160, 28);
+            this.comboBox1.Size = new System.Drawing.Size(108, 21);
             this.comboBox1.TabIndex = 2;
             // 
             // btnpagamento
             // 
             this.btnpagamento.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnpagamento.Location = new System.Drawing.Point(758, 308);
+            this.btnpagamento.Location = new System.Drawing.Point(492, 415);
+            this.btnpagamento.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnpagamento.Name = "btnpagamento";
-            this.btnpagamento.Size = new System.Drawing.Size(185, 39);
+            this.btnpagamento.Size = new System.Drawing.Size(123, 25);
             this.btnpagamento.TabIndex = 3;
             this.btnpagamento.Text = "Confirma pagamento";
             this.btnpagamento.UseVisualStyleBackColor = true;
@@ -74,32 +77,37 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(374, 317);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(219, 421);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(170, 20);
+            this.label1.Size = new System.Drawing.Size(137, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Metodo de pagamento";
+            this.label1.Text = "Metodo de pagamento:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(609, 13);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(406, 8);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(158, 20);
+            this.label2.Size = new System.Drawing.Size(124, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Lista de pagamentos";
             // 
             // FmGestaopagamentos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateBlue;
-            this.ClientSize = new System.Drawing.Size(1370, 707);
+            this.ClientSize = new System.Drawing.Size(913, 460);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnpagamento);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FmGestaopagamentos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gestão de Pagamentos";
--- SistemaHotel/FmLogin.Designer.cs
+++ SistemaHotel/FmLogin.Designer.cs
@@ -43,9 +43,10 @@
             // 
             this.user.AutoSize = true;
             this.user.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.user.Location = new System.Drawing.Point(179, 56);
+            this.user.Location = new System.Drawing.Point(119, 36);
+            this.user.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.user.Name = "user";
-            this.user.Size = new System.Drawing.Size(86, 25);
+            this.user.Size = new System.Drawing.Size(64, 17);
             this.user.TabIndex = 5;
             this.user.Text = "Usuário";
             // 
@@ -53,18 +54,20 @@
             // 
             this.txtusuario.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtusuario.Location = new System.Drawing.Point(73, 108);
+            this.txtusuario.Location = new System.Drawing.Point(49, 70);
+            this.txtusuario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtusuario.Name = "txtusuario";
-            this.txtusuario.Size = new System.Drawing.Size(299, 26);
+            this.txtusuario.Size = new System.Drawing.Size(201, 20);
             this.txtusuario.TabIndex = 1;
             // 
             // senha
             // 
             this.senha.AutoSize = true;
             this.senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.senha.Location = new System.Drawing.Point(185, 179);
+            this.senha.Location = new System.Drawing.Point(123, 116);
+            this.senha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.senha.Name = "senha";
-            this.senha.Size = new System.Drawing.Size(75, 25);
+            this.senha.Size = new System.Drawing.Size(54, 17);
             this.senha.TabIndex = 4;
             this.senha.Text = "Senha";
             // 
@@ -72,18 +75,20 @@
             // 
             this.txtsenha.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtsenha.Location = new System.Drawing.Point(73, 224);
+            this.txtsenha.Location = new System.Drawing.Point(49, 146);
+            this.txtsenha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtsenha.Name = "txtsenha";
             this.txtsenha.PasswordChar = '*';
-            this.txtsenha.Size = new System.Drawing.Size(299, 26);
+            this.txtsenha.Size = new System.Drawing.Size(201, 20);
             this.txtsenha.TabIndex = 2;
             // 
             // btnLogin
             // 
             this.btnLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnLogin.Location = new System.Drawing.Point(174, 282);
+            this.btnLogin.Location = new System.Drawing.Point(116, 183);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(104, 41);
+            this.btnLogin.Size = new System.Drawing.Size(69, 27);
             this.btnLogin.TabIndex = 3;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -99,17 +104,19 @@
             this.panel1.Controls.Add(this.senha);
             this.panel1.Controls.Add(this.btnLogin);
             this.panel1.Controls.Add(this.txtsenha);
-            this.panel1.Location = new System.Drawing.Point(370, 99);
+            this.panel1.Location = new System.Drawing.Point(247, 64);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(450, 432);
+            this.panel1.Size = new System.Drawing.Size(300, 281);
             this.panel1.TabIndex = 7;
             // 
             // btnCadastro
             // 
             this.btnCadastro.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnCadastro.Location = new System.Drawing.Point(174, 355);
+            this.btnCadastro.Location = new System.Drawing.Point(116, 231);
+            this.btnCadastro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCadastro.Name = "btnCadastro";
-            this.btnCadastro.Size = new System.Drawing.Size(104, 46);
+            this.btnCadastro.Size = new System.Drawing.Size(69, 30);
             this.btnCadastro.TabIndex = 8;
             this.btnCadastro.Text = "Cadastrar";
             this.btnCadastro.UseVisualStyleBackColor = true;
@@ -117,13 +124,14 @@
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1188, 660);
+            this.ClientSize = new System.Drawing.Size(792, 429);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
